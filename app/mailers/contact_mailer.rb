56b943e3ee7e8f5e163dbf417753974165af91b0
IceMupppet <romchai2003@gmail.com>
2016-03-31 00:04:32 +0000

    Added mailer and view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,10 @@
 class ContactMailer < ActionMailer::Base
     default to: 'rk120879@gmail.com'
-    
     def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
-        
-        mail(from: email, subject: 'Contact from Message')
+        mail(from: email, subject: 'Contact Form Message')
     end
 end
         
