ac77d5a2fa04b3dfd0fc7ea891ad4b615618626f
IceMupppet <romchai2003@gmail.com>
2016-04-08 05:25:50 +0000

    Added plan and stripe migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407012721) do
+ActiveRecord::Schema.define(version: 20160407085139) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160407012721) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
     t.string   "stripe_customer_token"
   end
 
